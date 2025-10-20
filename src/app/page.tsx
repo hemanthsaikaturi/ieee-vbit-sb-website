f6f8975079f0b6cc1c666f26b05b171b0c3b70b4
hemanth <hemanthsaikaturi@gmail.com>
2025-10-20 06:15:42 +0530

    refactor: remove unnecessary line break in Home component for cleaner code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,6 @@ export default function Home() {
       <AnimateOnScroll>
         <SocietiesSection />
       </AnimateOnScroll>
-      
       <AnimateOnScroll>
         <section className="py-16 bg-white">
           <div className="container mx-auto px-4">
