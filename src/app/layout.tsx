7d460d09bc22bc1f34eeb3a1d293364976d2ae9b
hemanth <hemanthsaikaturi@gmail.com>
2025-11-16 01:06:18 +0530

    feat: add structured data schema markup for improved SEO and organization representation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,48 @@ import Favicon from './favicon.png';
 import { ClientLayoutWrapper } from "@/components/ClientLayoutWrapper";
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://www.ieeevbitsb.in'),
+  alternates: {
+    canonical: '/',
+  },
   title: {
     default: 'IEEE - VBIT SB | Advancing Technology for Humanity',
     template: '%s | IEEE - VBIT SB',
   },
   description: "The official website for the IEEE Student Branch at Vignana Bharathi Institute of Technology.",
-  icons: [{ rel: 'icon', url: Favicon.src }], 
+  icons: [{ rel: 'icon', url: Favicon.src }],
+};
+
+// --- NEW SCHEMA MARKUP ---
+const jsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'Organization',
+  'name': 'IEEE VBIT Student Branch',
+  'alternateName': 'IEEE VBIT SB',
+  'url': 'https://www.ieeevbitsb.in',
+  'logo': 'https://www.ieeevbitsb.in/ieee-vbit-sb.png',
+  'contactPoint': {
+    '@type': 'ContactPoint',
+    'email': 'ieeevbitsbdoc@gmail.com',
+    'contactType': 'Customer Service',
+  },
+  'sameAs': [
+    'https://www.facebook.com/ieeevbitsb',
+    'https://www.instagram.com/ieee_vbitsb/',
+    'https://www.linkedin.com/company/ieee-vbit-sb',
+    'https://vbithyd.ac.in/ieee-student-branch-of-vbit/' 
+  ],
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="" suppressHydrationWarning={true}>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+
         <ClientLayoutWrapper>
           {children}
         </ClientLayoutWrapper>
